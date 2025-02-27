2aae81026c81a97cbfd6c64050406923524ca172
目棃 <BT-Muli@outlook.com>
2025-02-27 16:43:24 +0800

    🐛 fix build err

--- src/components/a-download.vue
+++ src/components/a-download.vue
@@ -6,7 +6,7 @@
 <script lang="ts" setup>
 import { computed } from "vue";
 
-type DownloadProps = { data: string; filename: string };
+type DownloadProps = { data?: UIGF4.Schema; filename: string };
 const props = defineProps<DownloadProps>();
-const downloadUrl = computed<string>(() => `data:text/json;charset=utf-8,${encodeURIComponent(props.data)}`);
+const downloadUrl = computed<string>(() => `data:text/json;charset=utf-8,${encodeURIComponent(JSON.stringify(props.data ?? {}))}`);
 </script>
