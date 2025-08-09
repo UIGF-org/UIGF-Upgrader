935df003555c766d65df58b39b5e99871f40081d
BTMuli <bt-muli@outlook.com>
2025-08-09 14:51:43 +0800

    🌱 使用图标&国际化

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
+/**
+ * @file main.ts
+ * @description 主入口
+ * @since 2.0.0
+ */
+
 import { createApp } from "vue";
 import App from "./App.vue";
+import i18n from "./i18n";
 import ArcoVue from "@arco-design/web-vue";
+import ArcoVueIcon from "@arco-design/web-vue/es/icon";
 import "@arco-design/web-vue/dist/arco.css";
 
-createApp(App).use(ArcoVue).mount("#app");
+createApp(App).use(ArcoVue).use(ArcoVueIcon).use(i18n).mount("#app");
